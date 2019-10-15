611687936f26fbc9ef5116334314493d4c25665c
Naimul Islam Arif <linarifux@gmail.com>
2019-10-15 23:03:51 +0600

    change in API platform

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ action.addEventListener('click', f => {
     var q = input.value;
     var KEY = "9e6fba673a9368e1f54a9d58ba97dc2f";
     if (input.value === "") {
-        window.alert("Please, enter a city name")
+        window.alert("Come on, Put a city name first")
     } else {
-        fetch(`http://api.weatherstack.com/current?access_key=${KEY}&query=${q}`)
+        const data = fetch(`http://api.weatherstack.com/current?access_key=${KEY}&query=${q}`)
             .then(response => response.json())
             .then(boom => {
+                console.log(boom);
+
                 city.innerHTML = `${boom.location.name}`;
                 temp.innerHTML = `${boom.current.temperature} &#8451;`;
                 wind.innerHTML = `${boom.current.wind_speed} km/h`;
