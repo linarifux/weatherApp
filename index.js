909635030e7d512115a6c6399ff728db98f444a2
Naimul Islam <linarifux@gmail.com>
2021-01-20 02:51:10 +0600

    Removed the day/nigh feature

--- index.js
+++ index.js
@@ -5,28 +5,31 @@ var temp = document.querySelector(".temp");
 var wind = document.querySelector(".wind");
 var dayNight = document.querySelector(".day-night");
 
-
-action.addEventListener('click', f => {
+ function myfunc (){
     var q = input.value;
     var KEY = "ba94c4d1e85eb7c3f7ead9e2427a4cd1";
     if (input.value === "") {
         window.alert("Please, enter a city name")
     } else {
-        const data = fetch(`api.openweathermap.org/data/2.5/weather?q=${q}&appid=${KEY}`)
+        const data = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${KEY}`)
             .then(response => response.json())
             .then(result => {
                 console.log(result);
 
                 city.innerHTML = `${result.name}`;
-                temp.innerHTML = `${result.main.temp}`;
+                temp.innerHTML = `${Math.round(result.main.temp - 273.15)} &#8451;`;
                 wind.innerHTML = `${result.wind.speed} km/h`;
-                // if (result.current.is_day === 0) {
-                //     dayNight.innerHTML = "Night";
-                // } else {
-                //     dayNight.innerHTML = "Day";
-                // }
             })
     }
 
     input.value = "";
+}
+
+
+action.addEventListener('click', myfunc)
+input.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13){
+        myfunc()
+    }
+
 })
